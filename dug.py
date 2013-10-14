e0c641850016b7598b8482bcfdf4e5f932fbde9f
Riley Moses <victi.resurgo@gmail.com>
2013-10-14 15:03:20 -0600

    Switch from argparse to optparse because Isengard uses Python 2.4.3

--- dug.py
+++ dug.py
@@ -1,7 +1,21 @@
 #! /usr/bin/env python
 
-import argparse
+# The machine this was designed to run on only had Python 2.4.3, so optparse is used instead of argparse
+import optparse
 
-parser = argparse.ArgumentParser(description = 'Basic dig implementation using Python')
-parser.add_argument('hostname', help = 'Name of the host to ask about')
-parser.add_argument('nameserver', help = 'IP address of the nameserver to query')
+
+def main():
+	# Parse command-line arguments
+	parser = optparse.OptionParser(description = 'Basic dig implementation using Python',
+		usage = "usage: %prog hostname nameserver")
+
+	(options, args) = parser.parse_args()
+
+	print options
+	print args
+
+
+# __name__ will be '__main__' if this code is being run directly (i.e. 'python dug.py')
+# If so, execute normally. Otherwise, this code is being imported into another module
+if __name__ == '__main__':
+	main()
