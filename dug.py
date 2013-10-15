8a84abe4a27922be1b0d6ee40e15a1717bb2b582
Riley Moses <victi.resurgo@gmail.com>
2013-10-15 15:57:05 -0600

    Parse response for A-type queries

--- dug.py
+++ dug.py
@@ -14,16 +14,16 @@ CLASS_IN = 1
 
 
 # No bin() in 2.4
-# Convert unsigned int n to binary representation that is numBits long
-# Most credit goes to http://stackoverflow.com/a/1519418/1693087
-def d2b(n, numBits):
+# Convert unsigned int n to binary representation, optionally specify number of bits
+# Based on http://stackoverflow.com/a/1519418/1693087
+def d2b(n, numBits = 0):
 	bStr = ''
 	if n < 0:
 		raise ValueError, "must be a positive integer"
-	if n > (2**numBits - 1):
+	if numBits and n > (2**numBits - 1):
 		raise ValueError, "not enough bits to represent " + str(n)
 	if n == 0:
-		return '0' * numBits
+		return '0' * numBits if numBits else '0'
 	while n > 0:
 		bStr = str(n % 2) + bStr
 		n = n >> 1
@@ -82,9 +82,10 @@ def buildPacket(hostname):
 
 	# A sequence of labels, where each label consists of a length byte followed by that number of bytes
 	for piece in hostname.split('.'):
-		# No part of the domain name may exceed the max value for a byte (255)
-		if len(piece) > 255:
-			raise ValueError, "part of the domain name exceeds the maximum allowed length (255)"
+		# IMPORTANT: Top two bits of the number must be '00' to indicate a label
+		# Therefore, no part of the domain name may exceed 63 bytes
+		if len(piece) > 63:
+			raise ValueError, "part of the domain name exceeds the maximum allowed length (63)"
 		# Length byte, "!B" identifier indicates network-formatted (big-endian) unsigned char (1 byte)
 		questionSegment += struct.pack("!B", len(piece))
 		# The bytes themselves
@@ -109,44 +110,54 @@ def buildPacket(hostname):
 
 
 def sendPacket(nameserver, packet):
-	print "Creating socket"
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	print "Sending packet"
 	sock.sendto(packet, (nameserver, PORT))
-	print "Receiving response"
+	print "Packet sent"
 	data, addr = sock.recvfrom(RECV_BUF)
 	print "Received:", repr(data)
 	return data
 
 
 def parseResponse(response):
+	# Trim the response as it is parsed, but keep a copy of the original
+	origResponse = response
 	# Parse header
 	# Identifier
-	(identifier,), response = struct.unpack("!H", response[:2]), response[2:]
+	identifier, response = struct.unpack("!H", response[:2])[0], response[2:]
+
 	# Flags
-	(flags,), response = struct.unpack("!H", response[:2]), response[2:]
+	flags, response = struct.unpack("!H", response[:2])[0], response[2:]
 	flags = d2b(flags, 16)
 	qr, opcode, aa, tc, rd, ra, z, rc = flags[0], flags[1:5], flags[5], flags[6], flags[7], flags[8], flags[9:12], flags[12:]
 	# Number of questions
-	(qdcount,), response = struct.unpack("!H", response[:2]), response[2:]
+	qdcount, response = struct.unpack("!H", response[:2])[0], response[2:]
 	# Number of answers
-	(ancount,), response = struct.unpack("!H", response[:2]), response[2:]
+	ancount, response = struct.unpack("!H", response[:2])[0], response[2:]
 	# Number of resource records
-	(nscount,), response = struct.unpack("!H", response[:2]), response[2:]
+	nscount, response = struct.unpack("!H", response[:2])[0], response[2:]
 	# Number of additional records
-	(arcount,), response = struct.unpack("!H", response[:2]), response[2:]
+	arcount, response = struct.unpack("!H", response[:2])[0], response[2:]
 
-	# Parse question
+	if DEBUG:
+		print "ID:", identifier
+		print "Truncated:", 'yes' if int(tc) else 'no'
+		print "Authoritative:", 'yes' if int(aa) else 'no'
+		print "Return code:", int(rc, 2), "- ERROR" if int(rc, 2) != 0 else ''
+		print "Answers:", ancount
+
+	# Parse question, same as when building the packet
+	# TODO: Loop qdcount times, assume only 1 question for now
 	question = ''
 	# Name
+	# TODO: Refactor into its own function for reuse
 	while True:
-		(qlen,), response = struct.unpack("!B", response[:1]), response[1:]
+		qlen, response = struct.unpack("!B", response[:1])[0], response[1:]
 		if qlen == 0:
 			break
 		question += '.' + response[:qlen] if len(question) > 0 else response[:qlen]
 		response = response[qlen:]
 	# Type
-	(qtype,), response = struct.unpack("!H", response[:2]), response[:2]
+	qtype, response = struct.unpack("!H", response[:2])[0], response[2:]
 	if qtype == TYPE['A']:
 		question += ', Type A'
 	elif qtype == TYPE['NS']:
@@ -154,13 +165,80 @@ def parseResponse(response):
 	elif qtype == TYPE['CNAME']:
 		pass
 	# Class
-	(qclass,), response = struct.unpack("!H", response[:2]), response[:2]
+	qclass, response = struct.unpack("!H", response[:2])[0], response[2:]
 	if qclass == CLASS_IN:
 		question += ', Class IN'
-	print question
+	
+	if DEBUG:
+		print "Question:", question
 
 	# Parse answer
+	# TODO: Loop ancount times, assume only 1 answer for now
+	answer = ''
+	# Name, variable length
+	checkName = d2b(struct.unpack("!H", response[:2])[0])
+	label = ''
+	# If the binary representation of first two bytes starts with '11', name points to a label
+	if checkName[:2] == '11':
+		response = response[2:]
+		offset = int(checkName[2:], 2)
+		if DEBUG:
+			print "First two bits of name are set, points to offset", offset
+		# Refer to original response, since the question section has been consumed already
+		offsetResponse = origResponse[offset:]
+		while True:
+			qlen, offsetResponse = struct.unpack("!B", offsetResponse[:1])[0], offsetResponse[1:]
+			if qlen == 0:
+				break
+			label += '.' + offsetResponse[:qlen] if len(label) > 0 else offsetResponse[:qlen]
+			offsetResponse = offsetResponse[qlen:]
+	# Otherwise, name is a label
+	else:
+		if DEBUG:
+			print "Name is a label"
+		while True:
+			qlen, response = struct.unpack("!B", response[:1])[0], response[1:]
+			if qlen == 0:
+				break
+			label += '.' + response[:qlen] if len(label) > 0 else response[:qlen]
+			response = response[qlen:]
+	answer += label
+			
+	if DEBUG:
+		print "Label at that offset:", label
 
+	# Type of the RDATA field
+	rtype, response = struct.unpack("!H", response[:2])[0], response[2:]
+	if rtype == TYPE['A']:
+		answer += ', Type A'
+	elif rtype == TYPE['NS']:
+		pass
+	elif rtype == TYPE['CNAME']:
+		pass
+
+	# Class of the RDATA field
+	rclass, response = struct.unpack("!H", response[:2])[0], response[2:]
+	if rclass == CLASS_IN:
+		answer += ', Class IN'
+
+	# Unsigned 32-bit integer specifying the TTL in seconds
+	ttl, response = struct.unpack("!I", response[:4])[0], response[4:]
+	answer += ', TTL ' + str(ttl)
+
+	# Unsigned 16-bit integer specifying the length of the RDATA field
+	rdlen, response = struct.unpack("!H", response[:2])[0], response[2:]
+
+	# Variable-length data (depending on type of record) for the resource
+	if rtype == TYPE['A']:
+		try:
+			ip = socket.inet_ntoa(response)
+			answer += ', IP ' + ip
+		except socket.error:
+			print "Incorrect format for A-type RDATA"
+	elif rtype == TYPE['NS']:
+		pass
+
+	print "Answer:", answer
 
 
 def main():
